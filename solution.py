1118168263a875cbe8d40dde6650fb3df8c676db
Rockstaru <slupeasaurus@gmail.com>
2021-02-01 18:22:41 -0500

    Updating answer types for final 2 questions to numeric instead of string.

--- solution.py
+++ solution.py
@@ -19,9 +19,9 @@ def welcome_assignment_answers(question):
     elif question == "Is MD5 a secured hashing algorithm? Yes/No":
         answer = "No"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
-        answer = "5"
+        answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":
-        answer = "4"
+        answer = 4
     return (answer)
 
 
@@ -30,5 +30,5 @@ def welcome_assignment_answers(question):
 
 if __name__ == "__main__":
    # use this space to debug and verify that the program works
-   debug_question = "Test."
+   debug_question = "Are encoding and encryption the same? - Yes/No"
    print(welcome_assignment_answers(debug_question))
